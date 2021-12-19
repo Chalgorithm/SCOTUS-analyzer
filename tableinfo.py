db26f50b3c46dbce1fc3ad21b2f9f67c3fce515a
donierz <57650258+donierz@users.noreply.github.com>
2021-12-18 22:31:54 -0800

    added some links for solution

--- tableinfo.py
+++ tableinfo.py
@@ -2,19 +2,23 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-for page in range(21,5,-1):
-        
-        # response = requests.get("https://www.supremecourt.gov/opinions/slipopinion/"+str(page).zfill(2)) # zfill so if it is 5, it becomes 05, zfill of 2 makes sure length is 2.
-        url = 'https://www.supremecourt.gov/opinions/slipopinion/21'
 
-        r = requests.get(url)
+# https://stackoverflow.com/questions/47561116/scrape-webpage-containing-before
+# https://stackoverflow.com/questions/44704099/python-scrape-table-from-website
+# https://stackoverflow.com/questions/34250552/python-beautifulsoup-to-scrape-tables-from-a-webpage
+# https://stackoverflow.com/questions/41569480/scrape-tables-with-python
+
+for page in range(21,20,-1):
+        
+        r = requests.get("https://www.supremecourt.gov/opinions/slipopinion/"+str(page).zfill(2)) # zfill so if it is 5, it becomes 05, zfill of 2 makes sure length is 2.
+        
         html = r.text
 
         soup = BeautifulSoup(html)
-        table = soup.find('table', {"class": "t-chart"})
+        table = soup.find('table', {"class": "table table-bordered"})
         rows = table.find_all('tr')
         data = []
-        for row in rows[1:]:
+        for row in rows[0:]:
                 cols = row.find_all('td')
                 cols = [ele.text.strip() for ele in cols]
                 data.append([ele for ele in cols if ele])
