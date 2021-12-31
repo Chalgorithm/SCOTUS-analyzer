f7847b989892bc06e09cfd3fab9a414a320d614d
Chris Haley <cjhaley@gmail.com>
2021-12-31 12:18:39 -0800

    search scrape works now

--- search_scraper.py
+++ search_scraper.py
@@ -6,33 +6,32 @@ import re
 import json
 
 def scrape_search():
-    with open("data/search_scrapings.txt","a") as output_file:
-        opinion_table = pd.read_csv("data/table.csv")
-        for index, row in opinion_table.iterrows():
-            result = "None"
-            docket_number = row["Docket"]
+    output_file = open("data/search_scrapings.json","w")
+    resmap = []
+    opinion_table = pd.read_csv("data/table.csv")
+    for index, row in opinion_table.iterrows():
+        result = "None"
+        docket_number = row["Docket"]
+        try:
+            
+            # result = scrape_old_format(docket_number)
+            # print(result)
+            result = scrape_new_format(docket_number)
+            print({docket_number:result})
+        except Exception as e:
             try:
-
-                
-                # result = scrape_old_format(docket_number)
-                # print(result)
+                docket_number = row["Docket"]
                 result = scrape_new_format(docket_number)
                 print({docket_number:result})
-                output_file.write(docket_number +"->" +str(result)+"\n")
-            except Exception as e:
-                try:
-                    
-                    result = scrape_old_format(docket_number)
-                    print({docket_number:result})
-                    output_file.write(docket_number +"->"+str(result)+"\n")
-                except:
-                    print("can't get docket#")
-                print(e)
+            except:
+                print("can't get docket#")
+            print(e)
+        json.dump(resmap,output_file)
        
             
 
     #docket_number = "02-6683"
-    #new_docket_number = "19-897"
+    #new_docket_number = "21-463"
     #scrape_new_format(new_docket_number)
     
     #scrape_old_format(docket_number)
@@ -45,36 +44,27 @@ def scrape_new_format(docket_number):
     html = r.text
     soup = BeautifulSoup(html, features="lxml")
     proceedings_body = soup.find("table", id="proceedings")
-    '''
-    petition = proceedings_body.find('a', href=True, text="Petition", class_="documentanchor")
-    opinion = proceedings_body.find('a', href=True, text="opinion")
-    petitionlink = petition['href']
-    opinionlink = opinion['href']
-    
-    for row in proceedings_body.find_all("tr"):
-        proceedings = row.find_all("td")
-        content = proceedings[1].text.strip()
-    '''
+    proceedings = proceedings_body.find('a', href=True, text="Petition", class_="documentanchor")
+    link = proceedings['href']
     content = proceedings_body.text.strip()
     return result_scan(content)
+    # return result_scan(content)
    
 
 
 def scrape_old_format(docket_number):
     time.sleep(0.3)
     r = requests.get("https://www.supremecourt.gov/search.aspx?filename=/docketfiles/"+str(docket_number)+".htm")
-    print(r)
     html = r.text
     soup = BeautifulSoup(html, features="lxml")
     proceedings_body = soup.find("tbody")
     for row in proceedings_body.find_all("tr"):
-        print(row)
-        #proceedings = row.find_all("td")
-        #content = proceedings[1].strip()
+        proceedings = row.find_all("td")
+        content = proceedings[1].text
         return result_scan(content)
         
 def result_scan(content):
-    result_options = [r"AFFIRM",r"REMAND",r"VACATE",r"REVERSE",r"DISMISS"]
+    result_options = [r"AFFIRMED",r"REMANDED",r"VACATED",r"REVERSED",r"DISMISSED"]
     output_results = {}
     last_token = ""
     for token in content.split(" "):
@@ -85,14 +75,15 @@ def result_scan(content):
             if matches:
                 output_results[last_token] = r_i
         last_token = token
-
     return output_results
    
 
         
         
+            
+            
     
 
 
 if __name__ == "__main__":
-    print(scrape_old_format("14-212"))
+    scrape_search()
