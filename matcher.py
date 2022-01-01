14dae39b9d5f4aa1df0784838dd359f6080cdd9b
Chris Haley <cjhaley@gmail.com>
2021-12-31 16:00:12 -0800

    matcher update

--- matcher.py
+++ matcher.py
@@ -3,23 +3,42 @@ import pandas as pd
 import re
 import json
 from search_scraper import scrape_new_format,scrape_old_format
-
+# brief_mapping is a json dictionary maps a filename to a docket 
 brief_mapping = json.load(open("data/filename-to-docket.json"))
-
+#declare result mapping Docket# -> result information
 result_mapping = {}
 
+# these next 4 lines fill out result mapping with the current up-to-date data
 for line in open("data/search_scrapings.txt","r").read().split("\n"):
     split_line = line.split("->")
+    #layout docket# -> result info
     if len(split_line) > 1:
+        #without eval it is a string
         result_mapping[split_line[0]] = eval(split_line[1])
 
-def docket_match(LIWCfile,brief_reference,result_reference,opinionfile):
+def docket_match(LIWCdata,brief_reference,result_reference,tableinfodata):
+    #LIWCdata is dataframe with filename, and the liwc columns
+    #brief_reference is dictionary maps filename to docket#
+    # result_reference dictionary maps docket# to result
+    #tableinfodata scraped opinion information
     count = 0
-    LIWC = pd.read_csv(LIWCfile)
-    opinionfile = pd.read_csv(opinionfile)
+    #creates data frame liwc data
+    LIWC = pd.read_csv(LIWCdata)
+    tableinfofile = pd.read_csv(tableinfodata)
+
+    # D1 = dimension1 -> what row of the csv file you are on
     for D1,row in LIWC.iterrows():
-        for D2,cell in opinionfile.iterrows():
-            if(brief_reference[row["Filename"]] == cell["Docket"]):
+        for D2,cell in tableinfofile.iterrows():
+            #brief reference is brief_mapping
+            filename = row["Filename"]
+
+            if(brief_reference[filename] == cell["Docket"]):
+
+                #has to get into liwc table
+                result = str(result_reference[cell["Docket"]])
+                docket_number = str(cell["Docket"])
+                LIWC_filename = filename
+
                 count += 1
 
                 datboi = "LIWC file #"+str(D1)+" & scotus opinion #"+str(D2)+" matched on " + str(cell["Docket"])
--- search_scraper.py
+++ search_scraper.py
@@ -4,13 +4,21 @@ from bs4 import BeautifulSoup
 import time
 import re
 import json
+from itertools import islice
+
 
 def scrape_search():
     with open("data/search_scrapings.txt","a") as output_file:
         opinion_table = pd.read_csv("data/table.csv")
-        for index, row in opinion_table.iterrows():
+        startOfIndex = 350
+        for index, row in islice(opinion_table.iterrows(), startOfIndex, None):
             result = "None"
             docket_number = row["Docket"]
+            try:
+                print(str(docket_number)+"->"+str(scrape_any(docket_number)))
+            except Exception as e:
+                print(e)
+            """
             try:
 
                 
@@ -28,7 +36,7 @@ def scrape_search():
                 except:
                     print("can't get docket#")
                 print(e)
-       
+            """
             
 
     #docket_number = "02-6683"
@@ -57,7 +65,15 @@ def scrape_new_format(docket_number):
     '''
     content = proceedings_body.text.strip()
     return result_scan(content)
-   
+
+def scrape_any(docket_number):
+    time.sleep(0.3)
+    r = requests.get("https://www.supremecourt.gov/search.aspx?filename=/docketfiles/"+str(docket_number)+".htm")
+    print(r)
+    html = r.text
+    soup = BeautifulSoup(r.content, features="lxml")
+    #print(soup.contents)
+    return result_sentence(soup.contents)
 
 
 def scrape_old_format(docket_number):
@@ -72,6 +88,14 @@ def scrape_old_format(docket_number):
         proceedings = row.find_all("td")
         content = proceedings[1].strip()
         return result_scan(content)
+
+def result_sentence(pagetext):
+    regex = r"([A-Z][A-Za-z ,]*(AFFIRMED|REVERSED|REMANDED|VACATED|DISMISSED|DENIED)+([A-Za-z ,]*)\.)"
+    match = re.search(regex,str(pagetext))
+    if match != None:
+        m = match.group(1)
+        return m
+
 
         
 def result_scan(content):
