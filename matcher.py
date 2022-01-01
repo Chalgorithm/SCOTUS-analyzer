dc6b318cbf80670d6aba85d1fe8e6b38db1d3233
Chris Haley <cjhaley@gmail.com>
2021-12-31 16:24:26 -0800

    good stuff

--- matcher.py
+++ matcher.py
@@ -14,20 +14,35 @@ for line in open("data/search_scrapings.txt","r").read().split("\n"):
     #layout docket# -> result info
     if len(split_line) > 1:
         #without eval it is a string
-        result_mapping[split_line[0]] = eval(split_line[1])
+        result_mapping[split_line[0]] = str(split_line[1])
 
 def docket_match(LIWCdata,brief_reference,result_reference,tableinfodata):
     #LIWCdata is dataframe with filename, and the liwc columns
     #brief_reference is dictionary maps filename to docket#
     # result_reference dictionary maps docket# to result
     #tableinfodata scraped opinion information
-    count = 0
+    
     #creates data frame liwc data
     LIWC = pd.read_csv(LIWCdata)
     tableinfofile = pd.read_csv(tableinfodata)
-
+    
     # D1 = dimension1 -> what row of the csv file you are on
+    docket_number_column = []
+    result_column = []
     for D1,row in LIWC.iterrows():
+        filename = row["Filename"]
+        
+        try:
+            docket_number = brief_reference[filename]
+            result = result_reference[docket_number]
+            docket_number_column.append(docket_number)
+            result_column.append(result)
+        except Exception as e:
+            docket_number_column.append("")
+            result_column.append("")
+            print(e)
+        
+        '''
         for D2,cell in tableinfofile.iterrows():
             #brief reference is brief_mapping
             filename = row["Filename"]
@@ -49,12 +64,13 @@ def docket_match(LIWCdata,brief_reference,result_reference,tableinfodata):
                 except Exception as e:
                     print(e)
                 print(datboi)
-    print("count = "+str(count))
+            '''
+    #print(docket_number_column)
+    #print(result_column)
+    LIWC["docket_number"] = docket_number_column
+    LIWC["result"] = result_column
 
-
-
-         
-
+    LIWC.to_csv("data.csv",index=False)
 
 
 
