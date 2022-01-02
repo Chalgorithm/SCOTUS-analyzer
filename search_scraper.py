fbaf4284a1656f833f42c78ac725cbec2f6ca826
Alex Wang <alexw12@uci.edu>
2022-01-01 20:02:26 -0800

    search_scraper code cleanup
    title

--- search_scraper.py
+++ search_scraper.py
@@ -45,27 +45,6 @@ def scrape_search():
     
     #scrape_old_format(docket_number)
     
-
-def scrape_new_format(docket_number):
-    #get document
-    time.sleep(0.3)
-    r = requests.get("https://www.supremecourt.gov/search.aspx?filename=/docket/docketfiles/html/public/" + str(docket_number) + ".html")
-    html = r.text
-    soup = BeautifulSoup(html, features="lxml")
-    proceedings_body = soup.find("table", id="proceedings")
-    '''
-    petition = proceedings_body.find('a', href=True, text="Petition", class_="documentanchor")
-    opinion = proceedings_body.find('a', href=True, text="opinion")
-    petitionlink = petition['href']
-    opinionlink = opinion['href']
-    
-    for row in proceedings_body.find_all("tr"):
-        proceedings = row.find_all("td")
-        content = proceedings[1].text.strip()
-    '''
-    content = proceedings_body.text.strip()
-    return result_scan(content)
-
 def scrape_any(docket_number):
     time.sleep(0.3)
     r = requests.get("https://www.supremecourt.gov/search.aspx?filename=/docketfiles/"+str(docket_number)+".htm")
@@ -76,48 +55,12 @@ def scrape_any(docket_number):
     return result_sentence(soup.contents)
 
 
-def scrape_old_format(docket_number):
-    time.sleep(0.3)
-    r = requests.get("https://www.supremecourt.gov/search.aspx?filename=/docketfiles/"+str(docket_number)+".htm")
-    print(r)
-    html = r.text
-    soup = BeautifulSoup(html, features="lxml")
-    proceedings_body = soup.find("tbody")
-    for row in proceedings_body.find_all("tr"):
-        print(row)
-        proceedings = row.find_all("td")
-        content = proceedings[1].strip()
-        return result_scan(content)
-
 def result_sentence(pagetext):
     regex = r"([A-Z][A-Za-z ,]*(AFFIRMED|REVERSED|REMANDED|VACATED|DISMISSED|DENIED)+([A-Za-z ,]*)\.)"
     match = re.search(regex,str(pagetext))
     if match != None:
         m = match.group(1)
         return m
-
-
-        
-def result_scan(content):
-    result_options = [r"AFFIRM",r"REMAND",r"VACATE",r"REVERSE",r"DISMISS"]
-    output_results = {}
-    last_token = ""
-    for token in content.split(" "):
-        for r_i in range(0,len(result_options)):
-            expression = result_options[r_i]
-            matches = re.search(expression,token) is not None
-            # store dictionary of previous word and return
-            if matches:
-                output_results[last_token] = r_i
-        last_token = token
-
-    return output_results
-   
-
-        
-        
-    
-
 
 if __name__ == "__main__":
     scrape_search()
